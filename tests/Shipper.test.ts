6ead17c5e702bf550b205b4272b2ea692c95748d
Tobi <crowniedavid@myself.com>
2020-01-24 21:32:34 +0000

    use MockRsyncHelper

--- tests/Shipper.test.ts
+++ tests/Shipper.test.ts
@@ -2,10 +2,15 @@ import Shipper from '../src/Shipper';
 import fs from 'fs-extra';
 import {dummyShipperConfig1} from './fixtures/shipperConfigs';
 import {MockSsh} from './mocks/MockSsh';
+import MockRsyncHelper from './mocks/MockRsyncHelper';
 
 const dummyProjectPath = __dirname + '/dummy-project';
 const shipperJson = dummyProjectPath + '/shipper.json';
-const shipper = new Shipper(dummyProjectPath, new MockSsh());
+const shipper = new Shipper(
+  dummyProjectPath,
+  new MockSsh(),
+  new MockRsyncHelper(),
+);
 
 const createShipperJsonFile = (data = dummyShipperConfig1) => {
   try {
@@ -15,6 +20,19 @@ const createShipperJsonFile = (data = dummyShipperConfig1) => {
   shipper.loadConfig();
 };
 
+const clearSshFolder = () => {
+  try {
+    fs.removeSync(__dirname + '/__ssh__');
+  } catch (e) {}
+  try {
+    fs.mkdirSync(__dirname + '/__ssh__');
+  } catch (e) {}
+};
+
+beforeEach(() => {
+  clearSshFolder();
+});
+
 describe('init', () => {
   it('initialise fresh project', () => {
     try {
@@ -22,7 +40,6 @@ describe('init', () => {
     } catch (e) {}
     shipper.init();
     expect(fs.existsSync(shipperJson)).toBeTruthy();
-
     const data = JSON.parse(fs.readFileSync(shipperJson).toString());
     expect(data).toMatchSnapshot();
   });
@@ -32,5 +49,11 @@ describe('deploy', () => {
   it('deploy', async () => {
     createShipperJsonFile();
     await shipper.deploy();
+    expect(
+      fs.existsSync(__dirname + '/__ssh__/dummy-project/dummy2.js'),
+    ).toBeTruthy();
+    expect(
+      fs.existsSync(__dirname + '/__ssh__/dummy-project/.dist/dummy.js'),
+    ).toBeTruthy();
   });
 });
