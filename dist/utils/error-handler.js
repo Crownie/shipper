a52606a9403659b13435c45f72bfa55ae694f066
Tobi <stevehappyboy@rocketmail.com>
2020-04-11 13:41:55 +0100

    dist: del

--- dist/utils/error-handler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', {value: true});
-var errorHandler = function(err, req, res, next) {
-  console.log('internal server error: ');
-  console.log(err);
-  res.status(err.status || 500).send({error: err});
-};
-exports.default = errorHandler;
